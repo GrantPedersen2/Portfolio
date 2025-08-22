c99b13a14aacfb0a458176109c13510d69bae1f6
Grant Pedersen <grantpedersen2@live.com>
2025-08-22 15:17:34 -0700

    Added Glass Boxes and Grid format to the content of each blade

--- src/app/lib/data.jsx
+++ src/app/lib/data.jsx
@@ -7,45 +7,116 @@ const data =[
         title:"RESUME",
         detail:(
             <div className={"right-sidebar-grid"}>
-                <header className={"header"}>header</header>
+                <header className={"header"}>
+                    <div className={"glass-box-grid"}>
+                        <glass-header className={"glass-header"}>
+                            Grant Pedersen
+                        </glass-header>
+                        <glass-body-content className={"glass-body-content"}>
+                            THIS IS BODY
+                        </glass-body-content>
+                        <glass-body-right-content className={"glass-body-right-content"}>
+                            THIS IS RIGHT SIDE
+                        </glass-body-right-content>
+                    </div>
+                </header>
                 <main className={"main"}>Main Content</main>
                 <section className={"right-sidebar"}><div className={"glass-rectangle"}/></section>
                 <footer className={"footer"}>footer</footer>
-            </div>
-        ),
-        footer:"footer"
+            </div>),
+        footer:""
     },
     {
         keyID: 2,
         idx:2,
         id:"profile-blade",
         title:"PROFILE",
-        detail:"DETAIL",
-        footer:"footer"
+        detail:(
+        <div className={"right-sidebar-grid"}>
+            <header className={"header"}>
+                <div className={"glass-box-grid"}>
+                    <glass-header className={"glass-header"}>
+                        Grant Pedersen
+                    </glass-header>
+                    <glass-body-content className={"glass-body-content"}>
+                        THIS IS BODY
+                    </glass-body-content>
+                    <glass-body-right-content className={"glass-body-right-content"}>
+                        THIS IS RIGHT SIDE
+                    </glass-body-right-content>
+                </div>
+            </header>
+            <main className={"main"}>Main Content</main>
+            <section className={"right-sidebar"}><div className={"glass-rectangle"}/></section>
+            <footer className={"footer"}>footer</footer>
+        </div>),
+        footer:""
     },
     {
         keyID: 3,
         idx:3,
         id:"projects-blade",
         title:"PROJECTS",
-        detail:"DETAIL",
-        footer:"footer"
+        detail:(
+            <div className={"right-sidebar-grid"}>
+                <header className={"header"}>
+                    <div className={"glass-box-grid"}>
+                        <glass-header className={"glass-header"}>
+                            Grant Pedersen
+                        </glass-header>
+                        <glass-body-content className={"glass-body-content"}>
+                            THIS IS BODY
+                        </glass-body-content>
+                        <glass-body-right-content className={"glass-body-right-content"}>
+                            THIS IS RIGHT SIDE
+                        </glass-body-right-content>
+                    </div>
+                </header>
+                <main className={"main"}>Main Content</main>
+                <section className={"right-sidebar"}><div className={"glass-rectangle"}/></section>
+                <footer className={"footer"}>footer</footer>
+            </div>),
+        footer:""
     },
     {
         keyID: 4,
         idx:4,
         id:"media-blade",
         title:"MEDIA",
-        detail:"DETAIL",
-        footer:"footer"
+        detail:(
+            <div className={"right-sidebar-grid"}>
+                <header className={"header"}>
+                    <div className={"glass-box-grid"}>
+                        <glass-header className={"glass-header"}>
+                            Grant Pedersen
+                        </glass-header>
+                        <glass-body-content className={"glass-body-content"}>
+                            THIS IS BODY
+                        </glass-body-content>
+                        <glass-body-right-content className={"glass-body-right-content"}>
+                            THIS IS RIGHT SIDE
+                        </glass-body-right-content>
+                    </div>
+                </header>
+                <main className={"main"}>Main Content</main>
+                <section className={"right-sidebar"}><div className={"glass-rectangle"}/></section>
+                <footer className={"footer"}>footer</footer>
+            </div>),
+        footer:""
     },
     {
         keyID: 5,
         idx:5,
         id:"system-blade",
         title:"SYSTEM",
-        detail:"DETAIL",
-        footer:"footer"
+        detail:(
+            <div className={"right-sidebar-grid"}>
+                <header className={"header"}>header</header>
+                <main className={"main"}>Main Content</main>
+                <section className={"right-sidebar"}><div className={"glass-rectangle"}/></section>
+                <footer className={"footer"}>footer</footer>
+            </div>),
+        footer:""
     }
 ];
 export default data;
